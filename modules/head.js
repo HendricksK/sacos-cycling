f57b5e54aa3a572b06682d4b2e8e84b68525065a
kurvinh <kurvinhendricks@gmail.com>
2021-02-16 18:40:01 +0200

    second

---
+++ modules/head.js
@@ -0,0 +1,11 @@
+let head = document.head.innerHTML
+head = head 
+	+ '<meta charset="UTF-8">'
+	+ '<title>Sacos Cycling</title>'
+	+ '<a rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">'
+	+ '<a rel="stylesheet" href="../assets/css/bulma.min.css">'
+	+ '<a rel="stylesheet" href="../assets/css/base.css">'
+	+ '<a rel="stylesheet" href="../assets/css/overrides.css">'
+	+ '<a rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@creativebulma/bulma-collapsible@1.0.3/dist/css/bulma-collapsible.min.css">'
+
+document.head.innerHTML = head
