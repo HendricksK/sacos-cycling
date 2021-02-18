bb53dd1e58486a15ebf491060f633b5a040b58ca
kurvinh <kurvinhendricks@gmail.com>
2021-02-18 22:56:08 +0200

    ghpages set base due to server config

--- modules/head.js
+++ modules/head.js
@@ -1,12 +1,11 @@
 let head = document.head.innerHTML
 head = head 
 	+ '<meta charset="UTF-8">'
-	+ '<base href="/sacos-cycling/">'
 	+ '<title>Sacos Cycling</title>'
 	+ '<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">'
-	+ '<link rel="stylesheet" href="../assets/css/bulma.min.css">'
-	+ '<link rel="stylesheet" href="../assets/css/base.css">'
-	+ '<link rel="stylesheet" href="../assets/css/overrides.css">'
+	+ '<link rel="stylesheet" href="/sacos-cycling/assets/css/bulma.min.css">'
+	+ '<link rel="stylesheet" href="/sacos-cycling/assets/css/base.css">'
+	+ '<link rel="stylesheet" href="/sacos-cycling/assets/css/overrides.css">'
 	+ '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@creativebulma/bulma-collapsible@1.0.3/dist/css/bulma-collapsible.min.css">'
 	+ '<link href="https://unpkg.com/vueperslides/dist/vueperslides.css" rel="stylesheet">'
 	+ '<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>'
