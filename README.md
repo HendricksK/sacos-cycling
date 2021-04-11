a07d77081e05c62126eb75b0b2aba09a7bd35db0
kurvinh <kurvinhendricks@gmail.com>
2021-04-11 23:43:24 +0200

    rendering a single article template

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## TODO: setup proper module loading for JS modules
 ## TODO: use module helpers in all across JS modules
-
+## TODO: build out import upload tool
+## TODO: build out correction request form/tool
 
--- modules/pages/article/src/article.js
+++ modules/pages/article/src/article.js
@@ -1,6 +1,4 @@
 // https://hashnode.com/post/7-different-ways-to-make-ajax-calls-in-javascript-in-2019-cjr7pi2fl000gdjs2zgssqwhr
-// 
-
 import { getParameterByName } from "../../../lib/helper/src/url-helper.js";
 import { removeLoader } from "../../../lib/helper/src/loader-helper.js";
 
@@ -20,10 +18,33 @@ function home(callback) {
 
     xhr.onreadystatechange = (e) => {
         if (xhr.readyState === 4) {
-        	console.log(JSON.parse(xhr.responseText))
-            // renderHtml(JSON.parse(xhr.responseText))
+            renderHtml(JSON.parse(xhr.responseText))
             //Wait until data is done being retrieved then run callback
             callback()
         }
     }    
+}
+
+function renderHtml(data) {
+	const rider = data
+	const heading = rider.name
+	const article_data = rider.article_data
+	const author = rider.author
+	const date_time = rider.datetime
+
+	if (heading !== '') {
+        document.getElementById('article-header').innerHTML = heading
+    }
+
+    if (article_data !== '') {
+        document.getElementById('article-text').innerHTML = article_data
+    }
+
+    if (author !== '') {
+        document.getElementById('article-author').innerHTML = author
+    }
+
+    if (date_time !== '') {
+        document.getElementById('article-datetime').innerHTML = date_time
+    }
 }
