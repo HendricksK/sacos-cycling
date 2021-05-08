e25cbded960ef27c37f776b35716973d1914c74f
HendricksK <kurvinhendricks@gmail.com>
2021-05-09 00:34:28 +0200

    fix routes merge master into working branch

--- modules/config.js
+++ modules/config.js
@@ -7,7 +7,7 @@ if(url.includes('127.0.0.1')) {
 	window.localStorage.setItem('baseurl', 'http://127.0.0.1:8080/sacos-cycling')
 } else {
 	window.localStorage.setItem('apiurl', 'https://sacos-backend-go.herokuapp.com')
-	window.localStorage.setItem('baseurl', 'https://hendricksk.github.io/sacos-cycling')
+	window.localStorage.setItem('baseurl', 'https://cycling.sacoshistory.org')
 }
 
 window.localStorage.setItem('riderurl', '/pages/riders/rider/index.html')
