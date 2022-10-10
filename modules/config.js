1f0911196f16b2427f8ae7751ca82c496ec74ba2
HendricksK <kurvinhendricks@gmail.com>
2022-10-10 12:47:28 +0200

    adding 404 and maintenance pages for SEO and future back-end upgrades

--- modules/config.js
+++ modules/config.js
@@ -4,7 +4,7 @@ let url = window.location.href
 
 if(url.includes('127.0.0.1')) {
 	window.localStorage.setItem('apiurl', 'http://localhost:6660')
-	window.localStorage.setItem('baseurl', 'http://127.0.0.1:8080/sacos-cycling')
+	window.localStorage.setItem('baseurl', 'http://127.0.0.1:8080')
 } else {
 	window.localStorage.setItem('apiurl', 'https://sacos-backend-go.herokuapp.com')
 	window.localStorage.setItem('baseurl', 'https://cycling.sacoshistory.org')
